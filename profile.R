ae500692b3b6c168df4cbad9cdc4f37c9a876d0a
cking921 <cking134@vols.utk.edu>
2024-04-02 20:26:46 -0400

    HW2 commit

--- profile.R
+++ profile.R
@@ -1,7 +1,44 @@
-x = matrix(runif(1e5*500), ncol = 500)
+Rprof()
+  
+system.time({
+ntrials <- 3e4
+counter <- 0
+exam.results <- data.frame(matrix(ncol=10, nrow=0),row.names = NULL)
+
+set.seed(583)
+for (trial in 1:ntrials) {
+  exam <- sample(1:50, 10, replace = FALSE)
+  exam.results <- rbind(exam.results, exam)
+}
+
+counter <- sum(rowSums(exam.results <= 30) >= 7)
+prob1 <- counter / ntrials
+
+})
+
+Rprof(NULL)
+summaryRprof()
+
 
 
 Rprof()
-x_pr = prcomp(x, retx = FALSE)
+
+system.time({
+set.seed(583)
+ntrials <- 3e4
+
+exam_results_rep <- do.call("rbind", 
+                            replicate(ntrials, 
+                                      {sample(1:50, 10, replace = FALSE)}, 
+                                      simplify = FALSE))
+
+counter <- sum(rowSums(exam_results_rep <= 30) >= 7)
+prob2 <- counter / ntrials
+})
+
 Rprof(NULL)
 summaryRprof()
+
+
+
+all.equal(prob1, prob2)
